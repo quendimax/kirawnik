b541311dbed168807184313f00e9b69b05b3efb0
Максім Тамковіч <quendimax@tut.by>
2011-06-22 12:43:13 +0300

    Add methods of Directory class

--- src/fs/Directory.h
+++ src/fs/Directory.h
@@ -1,11 +1,27 @@
 #ifndef __FS_DIRECTORY_H__
 #define __FS_DIRECTORY_H__
 
+#include "FileInfo.h"
+
 
 class Directory
 {
+	Directory();
+	Directory(const Directory &);
+	explicit Directory(const char *dirName);
+	~Directory();
+
+	const FileInfo &fileInfo() const;
+	int count() const;
+
+	bool cd(const char *newFolder);
+	bool cdUp();
+	const char *dirName() const;
+
+	Directory &operator=(const Directory &);
+	const char *operator[](int index) const;
+
 };
 
 
 #endif //__FS_DIRECTORY_H__
-
