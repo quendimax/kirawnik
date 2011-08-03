5c73a68d3bdd6d65f3fded6b21161e4d2c6527c8
Максім Тамковіч <quendimax@tut.by>
2011-08-03 16:56:29 +0300

    Added:   * the FilePanel class

---
+++ src/app/FilePanel.h
@@ -0,0 +1,20 @@
+#ifndef __FILEPANEL_H__
+#define __FILEPANEL_H__
+
+#include <QtGui/QWidget>
+
+
+/*!
+  The FilePanel object shows information about curent disk part,
+  and curent directory.
+  */
+class FilePanel : public QWidget
+{
+	Q_OBJECT
+
+public:
+	explicit FilePanel(QWidget *parent = 0);
+};
+
+
+#endif //__FILEPANEL_H__
