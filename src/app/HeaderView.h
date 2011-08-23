d79b4b660ff74bb1cfb7c77a207c22a4fcb98642
Максім Тамковіч <quendimax@tut.by>
2011-08-23 20:09:45 +0300

    Release moving of headers. Move and rename ItemFlag to SortFlag in Global.h

--- src/app/HeaderView.h
+++ src/app/HeaderView.h
@@ -5,6 +5,8 @@
 #include <QList>
 #include <QWidget>
 
+#include "Global.h"
+
 class QMenu;
 class QPainter;
 class QStyleOption;
@@ -26,18 +28,6 @@ protected:
 	void paintEvent(QPaintEvent *);
 
 private:
-	void initMenu();
-	void destroyMenu();
-	void initStyleOption(QStyleOption *option);
-	void paintSection(int index, QPainter &painter);
-	void updateAll();
-
-	int indexAt(const QPoint &pos, bool *isResize = 0);
-
-	void readSettings();
-	void writeSettings();
-
-private:
 	enum HeaderState {
 		HS_Free,           //!< mouse buttons is raisen
 		HS_Pressing,       //!< mouse left button is pressing on not control items
@@ -46,46 +36,45 @@ private:
 		HS_Moving          //!< mouse left button is pressing on header item
 	};
 
-	enum ItemFlag {
-		Item_Name = 0x01,
-		Item_Suffix = 0x02,
-		Item_Size = 0x04,
-		Item_TextPerms = 0x08,
-		Item_DigitPerms = 0x10,
-		Item_Owner = 0x20,
-		Item_Group = 0x40,
-		Item_Modified = 0x80,
-
-		Item_None = 0,
-		Item_All = 0xFF
-	};
-	typedef QFlags<ItemFlag> ItemFlags;
-
 	struct HeaderItem
 	{
 		int offset;
 		int width;
-		HeaderView::ItemFlag type;
+		Krw::SortFlag type;
 		QString name;
 
 		inline bool operator < (const HeaderItem &item) const { return offset < item.offset; }
 	};
 
 private:
+	void initMenu();
+	void destroyMenu();
+	void initStyleOption(QStyleOption *option);
+	void paintSection(int index, QPainter &painter);
+	void paintMovableSection(QPainter &painter);
+	void updateAll();
+
+	int indexAt(const QPoint &pos, bool *isResize = 0) const;
+	int indexAt(Krw::SortFlag flag) const;
+
+	void readSettings();
+	void writeSettings();
+
+private:
 	static QList<HeaderView *> s_headerViews;
 	static HeaderItem s_items[];
-	static ItemFlags s_showItems;
+	static Krw::SortFlags s_showItems;
 	static int s_showItemCount;
 	static int s_itemCount;
 	static int s_objectCount;
 	static QMenu *s_menu;
+	static HeaderItem s_movableItem;
 
 	int m_objectNumber;
-
 	QPoint m_oldPos;
 	HeaderState m_headerState;
-	ItemFlag m_sortingItem;
-	int m_pressedItem;
+	Krw::SortFlag m_sortingItem;
+	Krw::SortFlag m_pressedItem;
 	int m_resizeItem;
 	bool m_reverseSorting;
 };
