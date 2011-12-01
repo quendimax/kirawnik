c26d4b6eac38ceea6e72d653642daad84611e9a8
Максім Тамковіч <quendimax@tut.by>
2011-12-01 12:28:41 +0300

    Fixed bug: load the 512x512 icon

--- src/libs/core/Application.cpp
+++ src/libs/core/Application.cpp
@@ -14,7 +14,7 @@ Application::Application(int &argc, char **argv)
 	QApplication::setApplicationVersion(KIRAWNIK_VERSION);
 	QApplication::setOrganizationName("Violators Software");
 
-	setWindowIcon(QIcon(kApp->applicationDirPath() + "/../share/icons/hicolor/512x512/apps/kirawnik.png"));
+	setWindowIcon(QIcon(kApp->applicationDirPath() + "/../share/icons/hicolor/256x256/apps/kirawnik.png"));
 
 	m_settings = new QSettings(QSettings::IniFormat, QSettings::UserScope,
 	                           QApplication::organizationName(),
